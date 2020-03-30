60f726c25e3e48eeb9b731a167c37415893b9eb6
Tomohiro-Yamamoto201 <tomohiro.y201@gmail.com>
2020-03-31 00:24:23 +0900

    users/signupの編集

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root 'users#index'
   get 'users/show'
   get 'users/new'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
