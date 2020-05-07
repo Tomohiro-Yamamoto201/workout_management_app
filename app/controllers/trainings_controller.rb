8d5421e70fe5c9f7143a0bffa87df37ed2f247d4
Tomohiro-Yamamoto201 <tomohiro.y201@gmail.com>
2020-05-08 00:45:13 +0900

    カレンダー機能2

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,2 +1,43 @@
 class TrainingsController < ApplicationController
+    def index
+        @trainings = Training.all
+      end
+    
+      def new
+        @trainings = Training.new
+      end
+    
+      def show
+        @trainings = Training.find(params[:id])
+      end
+    
+      def create
+        Training.create(Training_parameter)
+        redirect_to Trainings_path
+      end
+    
+      def destroy
+        @training = Training.find(params[:id])
+        @training.destroy
+        redirect_to trainings_path, notice:"削除しました"
+      end
+    
+      def edit
+        @training = Training.find(params[:id])
+      end
+    
+      def update
+        @training = Training.find(params[:id])
+        if @training.update(training_parameter)
+          redirect_to trainings_path, notice: "編集しました"
+        else
+          render 'edit'
+        end
+      end
+    
+      private
+    
+      def training_parameter
+        params.require(:Training).permit(:title, :content, :start_time)
+      end
 end
