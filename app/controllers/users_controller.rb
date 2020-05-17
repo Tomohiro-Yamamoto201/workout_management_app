1e6ae2075a79532bf3c722654fd3697ca21d19ea
Tomohiro-Yamamoto201 <tomohiro.y201@gmail.com>
2020-05-18 01:54:20 +0900

    カレンダー機能2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,52 +1,12 @@
 class UsersController < ApplicationController
 
-  # def index
-  # end
-
-  # def new
-    
-  # end
-
   def index
-    @trainings = Training.all
   end
 
   def new
-    @trainings = Training.new
+    
   end
 
-  def show
-    @trainings = Training.find(params[:id])
-  end
-
-  def create
-    Training.create(Training_parameter)
-    redirect_to Trainings_path
-  end
-
-  def destroy
-    @training = Training.find(params[:id])
-    @training.destroy
-    redirect_to trainings_path, notice:"削除しました"
-  end
-
-  def edit
-    @training = Training.find(params[:id])
-  end
-
-  def update
-    @training = Training.find(params[:id])
-    if @training.update(training_parameter)
-      redirect_to trainings_path, notice: "編集しました"
-    else
-      render 'edit'
-    end
-  end
-
-  private
-
-  def training_parameter
-    params.require(:Training).permit(:title, :content, :start_time)
-  end
+  
 
 end
