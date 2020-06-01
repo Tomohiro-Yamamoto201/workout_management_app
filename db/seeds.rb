b761e48bf2924a0c3fd4f1895d846acbd284df82
Tomohiro-Yamamoto201 <tomohiro.y201@gmail.com>
2020-06-02 01:19:58 +0900

    管理ユーザー作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(nickname):  "Example User",
+User.create!(nickname:  "Example User",
              email: "example@railstutorial.org",
+             gender: "男",
+             height: "170",
+             body_weight: "60",
+             born_on: "20000401",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true)
