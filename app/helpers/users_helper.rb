7db61a9ad633958cdc0a1a3b8c7a84621579a6d5
Tomohiro-Yamamoto201 <tomohiro.y201@gmail.com>
2020-06-05 01:32:14 +0900

    ユーザー削除実装

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,23 @@
 module UsersHelper
+
+   # 渡されたユーザーをログイン
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 永続セッションとしてユーザーを記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+
+    # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+
+  
+
 end
