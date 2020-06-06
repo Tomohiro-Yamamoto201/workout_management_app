9e8e64e6b2acb390e77ef17c09d6c8a32a58c0dd
Tomohiro-Yamamoto201 <tomohiro.y201@gmail.com>
2020-06-06 11:11:58 +0900

    ユーザー削除実装

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -18,6 +18,10 @@ module UsersHelper
     user == current_user
   end
 
-  
+#   def current_user
+#     if session[:user_id]
+#         @current_user ||= User.find_by(id: session[:user_id])
+#     end
+#   end
 
 end
