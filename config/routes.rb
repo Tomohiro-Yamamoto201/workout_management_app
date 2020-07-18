bc8b4ddc0b083c964c03603d43d19911390c395e
Tomohiro-Yamamoto201 <tomohiro.y201@gmail.com>
2020-07-18 13:42:12 +0900

    training_menusTBのCRUD実装

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     omniauth:      'users/omniauth'
   }
   root 'users#index'
-  resources :user
+  resources :users
   resources :trainings
   resources :training_menus
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
