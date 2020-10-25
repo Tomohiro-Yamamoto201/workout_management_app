5882309bcc8b91b979d46962447c26e9f852b99c
Tomohiro-Yamamoto201 <tomohiro.y201@gmail.com>
2020-10-25 23:54:30 +0900

    likesTB作成

--- app/models/training_report.rb
+++ app/models/training_report.rb
@@ -1,5 +1,6 @@
 class TrainingReport < ApplicationRecord
   belongs_to :user
+  has_many :liked_users, through: :likes, source: :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
