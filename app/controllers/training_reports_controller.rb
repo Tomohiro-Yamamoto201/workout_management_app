bf4bb360e8e3f106f74ab3b9210497756b6ba39d
Tomohiro-Yamamoto201 <tomohiro.y201@gmail.com>
2020-10-28 22:54:45 +0900

    投稿画面作成

--- app/controllers/training_reports_controller.rb
+++ app/controllers/training_reports_controller.rb
@@ -1,6 +1,18 @@
 class TrainingReportsController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
 
-
+    def index
+      @user = User.find(params[:id])
+      @training_reports = @user.training_reports.paginate(page: params[:page])
+    end
     
+    def show
+    
+    end
+    
+    def destroy
+    
+    end
+
+
 end
