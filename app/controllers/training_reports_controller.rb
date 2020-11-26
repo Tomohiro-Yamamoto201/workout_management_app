c8e07681b997208fe8b50f627ad0859c67f6ccb8
Tomohiro-Yamamoto201 <tomohiro.y201@gmail.com>
2020-11-26 23:34:06 +0900

    投稿画面のview作成

--- app/controllers/training_reports_controller.rb
+++ app/controllers/training_reports_controller.rb
@@ -12,12 +12,25 @@ class TrainingReportsController < ApplicationController
     end
 
     def create
-
+      @training_report = TrainingReport.new(training_report_parameter)
+      @training_report.user = current_user
+      if @training_report.save
+        redirect_to training_reports_path(current_user.id)
+        flash[:success] = "投稿が完了しました"
+      else
+        redirect_to training_reports_path(current_user.id)
+        flash[:danger] = "投稿に失敗しました"
+      end
     end
     
     def destroy
     
     end
 
+    private
+
+        def training_report_parameter
+            params.require(:training_report).permit(:content, :user_id)
+        end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,17 @@
 module SessionsHelper
     
-    
+      # 渡されたユーザーでログインする
+    def log_in(user)
+      session[:user_id] = user.id
+    end
+
+    # 現在ログイン中のユーザーを返す (いる場合)　current_userはdevise gemによって使用可能。
+
+
+    # ユーザーがログインしていればtrue、その他ならfalseを返す
+    def logged_in?
+      !current_user.nil?
+    end
     
     # 記憶したURL (もしくはデフォルト値) にリダイレクト
     def redirect_back_or(default)
