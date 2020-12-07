cd30b50b91c5b7855e2108a80a8e8bed3575c55f
Tomohiro-Yamamoto201 <tomohiro.y201@gmail.com>
2020-12-07 23:43:40 +0900

    投稿画面のview作成

--- app/controllers/training_reports_controller.rb
+++ app/controllers/training_reports_controller.rb
@@ -1,12 +1,17 @@
 class TrainingReportsController < ApplicationController
     include Pagy::Backend
-    before_action :logged_in_user, only: [:create, :destroy]
+    before_action :logged_in_user, only: [:new, :create, :destroy]
 
     def index
       @user = User.find(params[:id])
       @pagy, @training_reports = pagy(TrainingReport.all)
     end
     
+    def new
+      # インスタンス変数を作成
+      @training_report = TrainingReport.new
+    end
+
     def show
         
     end
@@ -21,6 +26,7 @@ class TrainingReportsController < ApplicationController
         redirect_to training_reports_path(current_user.id)
         flash[:danger] = "投稿に失敗しました"
       end
+
     end
     
     def destroy
@@ -30,7 +36,8 @@ class TrainingReportsController < ApplicationController
     private
 
         def training_report_parameter
-            params.require(:training_report).permit(:content, :user_id)
+
+            params.require(:training_report).permit(:content)
         end
 
 end
